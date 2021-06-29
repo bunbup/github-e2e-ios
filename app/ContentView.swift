122d4d6c5c3d7ed18c4c42204fdd8acebf96f306
Mohit Tejani <mohit.tejani@pubnub.com>
2021-06-29 10:19:56 +0530

    * github workflow for tests in iPhone 11 &12. * Simple swift app with dummy tests.

---
+++ app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  app
+//
+//  Created by Mohit Tejani on 29/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ app/appApp.swift
@@ -0,0 +1,17 @@
+//
+//  appApp.swift
+//  app
+//
+//  Created by Mohit Tejani on 29/06/21.
+//
+
+import SwiftUI
+
+@main
+struct appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
